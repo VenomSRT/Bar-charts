6729aaf2d68c4eb511caf019e16174b90e6b1dbb
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-10-02 22:33:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+function setData(data) {
+  if (data.trim()) {
+    makeGraph(
+      data.split(' ')
+        .filter(elem => (elem.trim() !== '') && !isNaN(+elem.trim()))
+    );
+  }
+}
+
+function makeGraph(arr) {
+  const maxValue = Math.max(...arr);
+  const graphElem = document.getElementById('graph');
+
+  graphElem.innerHTML = '';
+
+  let elementHeight = null;
+  let elementColor = null;
+
+  arr.forEach(element => {
+    elementHeight = element / maxValue;
+    elementColor = Math.round(125 * (1 - elementHeight));
+
+    graphElem.innerHTML += (
+      `<div
+        class="pillar"
+        style="
+          height: ${Math.round(elementHeight * 100)}%;
+          background-color: hsl(${elementColor}, 100%, 50%)
+        "
+      >
+        <div>${element}</div>
+      </div>`
+    );
+  });
+}
+
+window.addEventListener('click', event => {
+  if (event.target.id === 'button') {
+    const inputValue = document.querySelector('#data-input').value;
+
+    setData(inputValue);
+  }
+});
